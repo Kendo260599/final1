c26d6c68b96312b08ea64b16863f6964c05c52a0
Kendo260599 <lienquanviet05@gmail.com>
2025-08-13 01:39:52 +0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
--- SQL schema for BirthInfo repository␊
-CREATE TABLE IF NOT EXISTS birth_info (␊
-    name TEXT PRIMARY KEY,␊
-    birth_date TEXT NOT NULL␊
+-- SQL schema for BirthInfo repository
+CREATE TABLE IF NOT EXISTS birth_info (
+    name TEXT PRIMARY KEY,
+    birth_date TEXT NOT NULL,
+    gender TEXT NOT NULL,
+    datetime_utc TEXT NOT NULL,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    source_note TEXT
 );
