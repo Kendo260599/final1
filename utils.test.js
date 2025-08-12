0c2f6c5c3a4512a2a950d509ff7c1b2ea885af3f
Kendo260599 <lienquanviet05@gmail.com>
2025-08-13 03:01:03 +0700

    Update utils.test.js

--- utils.test.js
+++ utils.test.js
@@ -1,7 +1,7 @@
 const test = require("node:test");
 const assert = require("node:assert");
 
-const parseDateParts = require("./parseDateParts");
+const parseDateParts = require("./parseDateParts.cjs");
 const { elemYear, elemConflict } = require("./elements");
 const {
   getEffectiveBirthYear,
