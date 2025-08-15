31c5b1d3f1d3384b090d69cf31872bbf397edf9d
Lê Thị Thanh Uyên <lienquanviet05@gmail.com>
2025-08-15 22:06:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ const app = express();
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname));
 
 app.get("/api/chart", (req, res) => {
   const { name } = req.query;
@@ -182,6 +181,7 @@ if (require.main === module) {
 }
 
 module.exports = app;
+
 
 
 
